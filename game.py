f148287325e961d86bb6c6692eaa57f27b0fea3f
Miacik <cacmiac@gmail.com>
2020-06-06 13:33:05 +0200

    Sanitization on x axis repaired 🛠🔧.

--- game.py
+++ game.py
@@ -1,14 +1,34 @@
 import numpy
+import math
 import random
 import sys
 import os
 sys.path.append(os.path.abspath("D:\\Miacik\\Projects\\2048-game\\lib"))
-from sanitize import sanitize
+from sanitize import *
+from helper import *
 
+#global variables
 board = [
-    [0,1,2,3],
+    [0,1,0,0],
     [0,4,5,4],
     [1,2,6,0],
     [1,2,5,4]
 ]
 
+powers_of_two = [math.pow(2, i) for i in range(12)]
+
+
+
+def moveXaxis(board):
+    new_board = sanitizex(board, 'right')
+    for i in range(len(new_board)):
+        for j in range(len(new_board) - 1):
+            if(new_board[i][j] in powers_of_two and new_board[i][j+1] == new_board[i][j]):
+                new_board[i][j+1] *= 2
+                delete(i,j,new_board)
+
+    board = new_board
+
+
+moveXaxis(board)
+print(board)
--- lib/sanitize.py
+++ lib/sanitize.py
@@ -1,13 +1,13 @@
 
 board = [
-    [0,1,2,3],
+    [0,1,0,0],
     [0,4,5,4],
     [1,2,6,0],
     [1,2,5,4]
 ]
 
 
-def sanitize(board, way=0):
+def sanitizex(board, way):
     """ 
     Function used to de-zero a list and append them in a way
     """
@@ -24,23 +24,32 @@ def sanitize(board, way=0):
             if(board[i][j] != 0):
                 cache.append((board[i][j], i, j))
 
-    for res in result:
-        for k in range(len(cache)):
-            if(result.index(res) == cache[k][1]):
-                res.append(cache[k][0])
 
+    print(cache)
+
+    for res in result:
+        index = result.index(res)
+        for k in range(len(cache)):
+            if(index == cache[k][1]):
+                res.append(cache[k][0])
+                
     if(way == "left"):
-        pass
+
+        for res in result:
+            while len(res) != 4:
+                res.append(0)
+        return result
+
     elif(way == "right"):
 
         for res in result:
-            if(len(res) != 4):
+            while len(res) != 4:
                 res.insert(0,0)
         return result
 
     else:
-        return "No other way is possible for now"
+        raise Exception("Choose right type, left or right !")
     
-   
+print(sanitizex(board, "right"))   
 
-print(sanitize(board))
+
